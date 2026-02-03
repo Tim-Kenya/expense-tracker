e573d6864bfeb51ae222c84083e904094fb9a08c
Tim-Kenya <171836577+Tim-Kenya@users.noreply.github.com>
2026-02-03 13:44:15 +0300

    feat(js): Initialize DOM element references for expense tracker

---
+++ script.js
@@ -0,0 +1,7 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionformEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
